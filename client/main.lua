54b73f20f9079edd10878d47d8df8a4380407bf7
Mr.N0BODY <51495170+N0BODY2@users.noreply.github.com>
2020-10-11 21:35:39 +0200

    Integrated with Inventoryhud

--- client/main.lua
+++ client/main.lua
@@ -22,57 +22,32 @@ end)
 
 function OpenShopMenu(zone)
 	local elements = {}
-	for i=1, #Config.Zones[zone].Items, 1 do
+	for i = 1, #Config.Zones[zone].Items, 1 do
 		local item = Config.Zones[zone].Items[i]
 
 		if item.limit == -1 then
 			item.limit = 100
 		end
 
-		table.insert(elements, {
-			label      = ('%s - <span style="color:green;">%s</span>'):format(item.label, _U('shop_item', ESX.Math.GroupDigits(item.price))),
-			label_real = item.label,
-			item       = item.item,
-			price      = item.price,
-
-			-- menu properties
-			value      = 1,
-			type       = 'slider',
-			min        = 1,
-			max        = item.limit
-		})
+		table.insert(
+			elements,
+			{
+				name = item.item,
+				label = item.label,
+				type = "item_standard",
+				usable = false,
+				rare = item.rare,
+				limit = -1,
+				price = item.price,
+				canRemove = false
+			}
+		)
 	end
 
+	TriggerEvent("esx_inventoryhud:openShop", zone, elements)
+
 	ESX.UI.Menu.CloseAll()
-	ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'shop', {
-		title    = _U('shop'),
-		align    = 'bottom-right',
-		elements = elements
-	}, function(data, menu)
-		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'shop_confirm', {
-			title    = _U('shop_confirm', data.current.value, data.current.label_real, ESX.Math.GroupDigits(data.current.price * data.current.value)),
-			align    = 'bottom-right',
-			elements = {
-				{label = _U('no'),  value = 'no'},
-				{label = _U('yes'), value = 'yes'}
-			}
-		}, function(data2, menu2)
-			if data2.current.value == 'yes' then
-				TriggerServerEvent('esx_blackmarket:buyItem', data.current.item, data.current.value, zone)
-			end
-
-			menu2.close()
-		end, function(data2, menu2)
-			menu2.close()
-		end)
-	end, function(data, menu)
-		menu.close()
-
-		CurrentAction     = 'shop_menu'
-		CurrentActionMsg  = _U('press_menu')
-		CurrentActionData = {zone = zone}
-	end)
-end
+end	
 
 AddEventHandler('esx_blackmarket:hasEnteredMarker', function(zone)
 	CurrentAction     = 'shop_menu'
